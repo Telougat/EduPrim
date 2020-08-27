60bbdcfb34a2c22af43de70c0fef438910eb9014
lorenzo <thelorenzo533@gmail.com>
2020-08-27 22:21:50 +0200

    Add helpers

---
+++ src/main/java/com/eduprim/servlets/Classes.java
@@ -0,0 +1,29 @@
+package com.eduprim.servlets;
+
+import com.eduprim.Helpers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "Classes", urlPatterns = {"/classes"})
+public class Classes extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (Helpers.userConnected(request))
+        {
+            this.getServletContext().getRequestDispatcher("/WEB-INF/classes.jsp").forward(request, response);
+        }
+        else
+        {
+            //TODO: send 404 page
+        }
+    }
+}
--- src/main/java/com/eduprim/servlets/Index.java
+++ src/main/java/com/eduprim/servlets/Index.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.Statement;
@@ -14,13 +15,17 @@ import java.sql.Statement;
 @WebServlet(name = "Index", urlPatterns = {"/"})
 public class Index extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mail = "mail";
+        String mdp = "MDP";
 
+        Adulte adulte = Adulte.connexion(mail, mdp);
+        if (adulte.getMail() != null && !adulte.getMail().isEmpty() && adulte.getStatus() != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", adulte);
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Adulte logger = Adulte.connexion("fpichon@cesi.fr", "123");
-        System.out.println("Log successful");
-        System.out.println(logger.getNom() + " " + logger.getPrenom() + " " + logger.getTelephone() + "  " + logger.getMail());
         this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 }

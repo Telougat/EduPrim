c083e6e8550dcd967fca70922f9afa2bc5337c87
lorenzo <thelorenzo533@gmail.com>
2020-08-27 21:05:26 +0200

    Add lasts beans

--- src/main/java/com/eduprim/beans/Cours.java
+++ src/main/java/com/eduprim/beans/Cours.java
@@ -1,7 +1,119 @@
 package com.eduprim.beans;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 public class Cours extends Database {
+    private Note note;
+    private Matiere matiere;
+    private Utilisateur utilisateur;
+    private Presence presence;
+    private Timestamp dateDebut;
+    private Timestamp dateFin;
+
     public Cours() {
         super(false);
+    }
+
+    public Cours(Note note, Matiere matiere, Utilisateur utilisateur, Presence presence, Timestamp dateDebut, Timestamp dateFin, boolean existe) {
+        super(true);
+        if (!existe) {
+            try {
+                PreparedStatement ps = this.connection.prepareStatement("INSERT INTO Cours(ID, ID_Matiere, ID_Utilisateurs, ID_Presences, DateDebut, DateFin) VALUES (?, ?, ?, ?, ?, ?)");
+                ps.setInt(1, note.getID());
+                ps.setInt(2, matiere.getID());
+                ps.setInt(3, utilisateur.getID());
+                ps.setInt(4, presence.getID());
+                ps.setTimestamp(5, dateDebut);
+                ps.setTimestamp(6, dateFin);
+                ps.executeUpdate();
+                this.note = note;
+                this.matiere = matiere;
+                this.utilisateur = utilisateur;
+                this.presence = presence;
+                this.dateDebut = dateDebut;
+                this.dateFin = dateFin;
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            this.note = note;
+            this.matiere = matiere;
+            this.utilisateur = utilisateur;
+            this.presence = presence;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+        }
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+
+    public Matiere getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(Matiere matiere) {
+        this.matiere = matiere;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Presence getPresence() {
+        return presence;
+    }
+
+    public void setPresence(Presence presence) {
+        this.presence = presence;
+    }
+
+    public Timestamp getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Timestamp dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Timestamp getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Timestamp dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public boolean save() {
+        if (this.utilisateur != null && this.presence != null)
+            return false;
+        if (this.connection == null)
+            this.getConnection();
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("UPDATE Cours SET DateDebut = ?, DateFin = ? WHERE ID_Presences = ? AND ID_Utilisateurs = ?");
+            ps.setTimestamp(1, this.dateDebut);
+            ps.setTimestamp(2, this.dateFin);
+            ps.setInt(3, this.presence.getID());
+            ps.setInt(4, this.utilisateur.getID());
+            int modifications = ps.executeUpdate();
+            if (modifications > 0)
+                return true;
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -42,40 +42,5 @@
                 </p>
             </div>
         </div>
-        <%--<div>
-            <div class="lg:flex lg:justify-center">
-                <div class="flex justify-center lg:flex-none">
-
-                </div>
-                <div class="flex justify-center lg:flex-none">
-                    <div class="w-4/5 lg:w-1/4 rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-                        <div style="font-family: Tahoma,sans-serif;" class="mb-4 text-6xl text-gris text-center">
-                            <h1>Vous êtes de retour !</h1>
-                        </div>
-                        <div class="mb-6">
-                            <label class="block text-grey-darker font-bold mb-1 text-md text-gris" for="identifiant">
-                                Identifiant
-                            </label>
-                            <input class="shadow appearance-none border-solid border border-gris rounded w-full py-2 px-3 text-grey-darker" id="identifiant" type="text" placeholder="">
-                        </div>
-                        <div class="mb-6">
-                            <label class="block text-grey-darker text-4xl lg:text-md font-bold mb-1 text-gris" for="mdp">
-                                Mot de passe
-                            </label>
-                            <input class="shadow appearance-none border border-gris rounded w-full py-2 px-3 text-grey-darker mb-3" id="mdp" type="password" placeholder="">
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <button class="bg-violet hover:bg-blue-dark text-blanc font-bold py-2 px-4 rounded w-full" type="button">
-                                Se connecter
-                            </button>
-                        </div>
-                        <p class="text-sm">
-                            Vous ne possédez pas de compte ? <a class="text-violet" href="#">créer un compte</a>
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-        </div>--%>
     </body>
 </html>

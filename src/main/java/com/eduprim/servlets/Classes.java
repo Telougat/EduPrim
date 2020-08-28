ad00136e96bb66463b58b1ae6949e10459317721
lorenzo <thelorenzo533@gmail.com>
2020-08-28 13:57:07 +0200

    Fix classes lists

--- src/main/java/com/eduprim/servlets/Classes.java
+++ src/main/java/com/eduprim/servlets/Classes.java
@@ -23,6 +23,7 @@ public class Classes extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (Helpers.userConnected(request))
         {
+            Helpers.getSessionUser(request).initClasseList();
             if (request.getParameter("id") != null) {
                 int id = Integer.parseInt(request.getParameter("id"));
                 Classe classe = new Classe();

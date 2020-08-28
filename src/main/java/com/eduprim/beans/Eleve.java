5327aab9c04e36128453d8c93673a99b502ed9a1
lorenzo <thelorenzo533@gmail.com>
2020-08-28 16:40:44 +0200

    Add getAllEleves methods

--- src/main/java/com/eduprim/beans/Eleve.java
+++ src/main/java/com/eduprim/beans/Eleve.java
@@ -1,9 +1,7 @@
 package com.eduprim.beans;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Eleve extends Utilisateur {
     private String mailParent;
@@ -18,6 +16,17 @@ public class Eleve extends Utilisateur {
         this.mailParent = mail;
         this.telephoneParent = telephone;
         this.saveNewEleve();
+    }
+
+    public Eleve(int ID, String nom, String prenom, Date naissance, String addresse, Status status, String mail, String telephone) {
+        this.setID(ID);
+        this.setNom(nom);
+        this.setPrenom(prenom);
+        this.setDateDeNaissance(naissance);
+        this.setAdresse(addresse);
+        this.setStatus(status);
+        this.mailParent = mail;
+        this.telephoneParent = telephone;
     }
 
     public Eleve(Utilisateur utilisateur, String mail, String telephone) {
@@ -81,6 +90,31 @@ public class Eleve extends Utilisateur {
         }
     }
 
+    public static ArrayList<Eleve> getAllEleves() {
+        Database database = new Database(true);
+        Connection connection = database.getConnection();
+        ArrayList<Eleve> eleves = new ArrayList<>();
+        Status statusEleve = new Status();
+        statusEleve.findStatus("Eleve");
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM Eleve");
+            ResultSet result = ps.executeQuery();
+            while (result.next()) {
+                int ID = result.getInt("ID");
+                String nom = result.getString("Nom");
+                String prenom = result.getString("Prenom");
+                Date naissance = result.getDate("DateDeNaissance");
+                String addresse = result.getString("Adresse");
+                String mail = result.getString("Mail_Parent");
+                String telephone = result.getString("Telephone_Parent");
+                eleves.add(new Eleve(ID, nom, prenom, naissance, addresse, statusEleve, mail, telephone));
+           }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return eleves;
+    }
+
     public boolean save(boolean dontSaveUtilisateur) {
         if (this.getID() <= 0)
             return false;
@@ -91,13 +125,21 @@ public class Eleve extends Utilisateur {
         try {
             PreparedStatement ps = this.connection.prepareStatement("UPDATE Eleve SET Nom = ?, Prenom = ?, DateDeNaissance = ?, Adresse = ?, ID_Status = ?, Telephone_Parent = ?, Mail_Parent = ? WHERE ID = ?");
             ps.setString(1, this.getNom());
+            System.out.println(this.getNom());
             ps.setString(2, this.getPrenom());
+            System.out.println(this.getPrenom());
             ps.setDate(3, this.getDateDeNaissance());
+            System.out.println(this.getDateDeNaissance().toString());
             ps.setString(4, this.getAdresse());
+            System.out.println(this.getAdresse());
             ps.setInt(5, this.getStatus().getID());
+            System.out.println(this.getStatus().getID());
             ps.setString(6, this.telephoneParent);
+            System.out.println(this.telephoneParent);
             ps.setString(7, this.mailParent);
+            System.out.println(this.mailParent);
             ps.setInt(8, this.getID());
+            System.out.println(this.getID());
             int modifications = ps.executeUpdate();
             if (modifications > 0) {
                 return true;
--- src/main/java/com/eduprim/servlets/ProfesseurCreation.java
+++ src/main/java/com/eduprim/servlets/ProfesseurCreation.java
@@ -1,21 +1,80 @@
 package com.eduprim.servlets;
 
+import com.eduprim.Helpers;
+import com.eduprim.beans.Adulte;
+import com.eduprim.beans.Classe;
+import com.eduprim.beans.Eleve;
+import com.eduprim.beans.Status;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @WebServlet(name = "ProfesseurCreation", urlPatterns = {"/prof/creation"})
 public class ProfesseurCreation extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (Helpers.userConnected(request)) {
+            if (Helpers.getSessionUser(request).getStatus().getLabel().equals("Admin")) {
+                String nom = request.getParameter("nom");
+                String prenom = request.getParameter("prenom");
+                String mail = request.getParameter("mail");
+                String naissance = request.getParameter("naissance");
+                String motDePasse = request.getParameter("motDePasse");
+                String adresse = request.getParameter("adresse");
+                String telephone = request.getParameter("telephone");
+                System.out.println("OK !");
+                if (checkValue(nom) && checkValue(prenom) && checkValue(mail) && checkValue(naissance) && checkValue(motDePasse) && checkValue(adresse) && checkValue(telephone)) {
+                    System.out.println("PAssage !");
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                    Date parsed = null;
+                    java.sql.Date sqlDate = null;
+                    try {
+                        parsed = format.parse(naissance);
+                        sqlDate = new java.sql.Date(parsed.getTime());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    Status status = new Status();
+                    status.findStatus("Professeur");
+                    Adulte adulte = new Adulte(nom, prenom, sqlDate, adresse, status, Long.parseLong(telephone), mail, motDePasse);
+                    response.sendRedirect(request.getContextPath() + "/accueil");
+                }
+                else {
+                    this.getServletContext().getRequestDispatcher("/WEB-INF/View/creationProfesseur.jsp").forward(request, response);
+                }
+            }
+        }
+        else {
+            response.sendError(403);
+            return;
+        }
+    }
 
+    private boolean checkValue(String value) {
+        return !value.isEmpty() && value != null;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("grandbg", "background-1.png");
-        request.setAttribute("smallbg", "background-mobile-1.png");
-        this.getServletContext().getRequestDispatcher("/WEB-INF/View/creationProfesseur.jsp").forward(request, response);
+        if (Helpers.userConnected(request))
+        {
+            if (Helpers.getSessionUser(request).getStatus().getLabel().equals("Admin"))
+            {
+                request.setAttribute("grandbg", "background-1.png");
+                request.setAttribute("smallbg", "background-mobile-1.png");
+                this.getServletContext().getRequestDispatcher("/WEB-INF/View/creationProfesseur.jsp").forward(request, response);
+            }
+            else {
+                response.sendError(403);
+                return;
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/connexion");
+        }
     }
 }
